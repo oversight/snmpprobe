3b0ee597208e3b4333255197af2cce19b67b040f
Jeroen van der Heijden <jeroen@cesbit.com>
2022-02-18 17:35:17 +0100

    Upd test

--- test_snmp.py
+++ test_snmp.py
@@ -1,21 +1,11 @@
 import asyncio
 import unittest
 from lib.check import CHECKS
-from lib.check.base import Base
 from lib.check.base import make_check
 from lib.mib.mib_index import MibIndex
 from mibs.parser.grammar import MibsGrammar
 from mibs.parser.parse_tree import parse_tree
 from mibs.parser.utils import remove_comments_from_mib
-
-
-mi = MibIndex()
-
-NEW_CHECK_DEFINITIONS = {
-    check_name: make_check(mi, check_name, check_info)
-    for check_name, check_info in CHECKS.items()
-}
-
 
 TEST_HOSTCONFIG = {
     'probeConfig': {
@@ -63,56 +53,59 @@ class TestMibs(unittest.TestCase):
 class TestProbe(unittest.TestCase):
     def test_check(self):
         check_name = 'RFC1213_MIB ASN.1 MIB'
+        check = NEW_CHECK_DEFINITIONS[check_name]
 
         data = {
             'hostUuid': '',
             'checkName': check_name,
             'hostConfig': TEST_HOSTCONFIG,
         }
-        asyncio.run(Base.run(data, TEST_CREDENTIALS))
+        asyncio.run(check.run(data, {}))
 
     def test_check_snmpv3(self):
         check_name = 'RFC1213_MIB ASN.1 MIB'
+        check = NEW_CHECK_DEFINITIONS[check_name]
 
         data = {
             'hostUuid': '',
             'checkName': check_name,
             'hostConfig': TEST_HOSTCONFIG_V3,
         }
-        asyncio.run(Base.run(data, TEST_CREDENTIALS))
+        asyncio.run(check.run(data, {'credentials': TEST_CREDENTIALS}))
 
     def test_check_func(self):
         check_name = 'IF_MIB ASN.1 MIB'
+        check = NEW_CHECK_DEFINITIONS[check_name]
 
         data = {
             'hostUuid': '',
             'checkName': check_name,
             'hostConfig': TEST_HOSTCONFIG,
         }
-        asyncio.run(Base.run(data))
+        asyncio.run(check.run(data, {}))
 
     def test_snmp_func(self):
         check_name = 'CheckSnmpPing'
+        check = NEW_CHECK_DEFINITIONS[check_name]
 
         data = {
             'hostUuid': '',
             'checkName': check_name,
             'hostConfig': TEST_HOSTCONFIG,
         }
-        asyncio.run(Base.on_run_check(data))
+        asyncio.run(check.run(data, {}))
 
     def test_my_check(self):
         check_name = 'MyCheck'
         check_conf = {'types': []}
         check = make_check(mi, check_name, check_conf)
-        NEW_CHECK_DEFINITIONS[check_name] = check
 
         data = {
             'hostUuid': '',
             'checkName': check_name,
             'hostConfig': TEST_HOSTCONFIG,
         }
-        asyncio.run(Probe.on_run_check(data))
+        asyncio.run(check.run(data, {}))
 
     def test_my_check_1(self):
         check_name = 'MyCheck'
@@ -120,17 +113,22 @@ class TestProbe(unittest.TestCase):
             {'mib_obj': 'RFC1213-MIB::system', 'type_name': ''}
         ]}
         check = make_check(mi, check_name, check_conf)
-        NEW_CHECK_DEFINITIONS[check_name] = check
 
         data = {
             'hostUuid': '',
             'checkName': check_name,
             'hostConfig': TEST_HOSTCONFIG,
         }
-        asyncio.run(Probe.on_run_check(data))
+        asyncio.run(check.run(data, {}))
 
 
 if __name__ == '__main__':
+    mi = MibIndex()
+    NEW_CHECK_DEFINITIONS = {
+        check_name: make_check(mi, check_name, check_info)
+        for check_name, check_info in CHECKS.items()
+    }
+
     suite = unittest.TestSuite()
 
     suite.addTest(TestMibs('test_'))
